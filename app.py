d2e1c799f48d2f45d860fba4595f49f906df8e28
Kaikei-e <e_kaikei@protonmail.com>
2020-10-07 19:29:25 +0900

    defalut code

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/title/<title>')
+
+def title(title):
+    return render_template('index.html', title=title)
+
+@app.route('/hoge')
+
+def hoge():
+    return 'hoge!'
